bb469206c48709fc5067a6ec1657783a25ce936f
AliHossain01 <alihossaincse01@gmail.com>
2022-04-18 00:33:28 +0600

    homepage new design

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Service.css'
+import './Service.css';
+import { BsCamera } from "react-icons/bs";
+
+
 const Service = ({ service }) => {
     const { id, name, img, description, price } = service;
     const navigate = useNavigate();
@@ -9,7 +12,7 @@ const Service = ({ service }) => {
         navigate(`/service/${id}`);
     }
     return (
-        <div className='g-5 col-sm-12 col-md-6 col-lg-4'>
+        <div className='g-5 col-sm-12 col-md-6 col-lg-4 text-center'>
             <div className="card" style={{ width: "18rem" }}>
                 <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
@@ -18,7 +21,7 @@ const Service = ({ service }) => {
                     <p className="card-text"><small>{description}</small></p>
 
                     <div className='text-center'>
-                        <button onClick={() => navigateToServiceDetail(id)} className="btn btn-dark ">Invite Me</button>
+                        <button onClick={() => navigateToServiceDetail(id)} className="btn btn-outline-dark ">Invite Me <BsCamera className='icon' /></button>
                     </div>
                 </div>
             </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,6 +5,7 @@ import logo from '../../../images/logo.jpg'
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from './../../../firebase.init';
 import { signOut } from 'firebase/auth';
+import './Header.css';
 
 const Header = () => {
     const [user] = useAuthState(auth);
@@ -24,15 +25,15 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="home#services">Captures</Nav.Link>
-                            <Nav.Link href="home#portfolio">Portfolio</Nav.Link>
+                            <Nav.Link href="home#services" className='effects'>Captures</Nav.Link>
+                            <Nav.Link href="home#portfolio" className='effects'>Portfolio</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="blog">Blog</Nav.Link>
-                            <Nav.Link as={Link} to="about">About</Nav.Link>
-                            {user ? <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Sign out</button>
+                            <Nav.Link as={Link} to="blog" className='effects'>Blog</Nav.Link>
+                            <Nav.Link as={Link} to="about" className='effects'>About</Nav.Link>
+                            {user ? <button className='btn btn-link text-white effects text-decoration-none' onClick={handleSignOut}>Sign out</button>
                                 :
-                                <Nav.Link as={Link} to="login">Login</Nav.Link>}
+                                <Nav.Link as={Link} to="login" className='effects'>Login</Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
